dc4396fa9769b4f6649798e0901378b53b023f06
kanikachopra <kanika.chopra@internetbrands.com>
2018-10-16 13:13:21 -0700

    to make package compatible with C++11 compiler

--- setup.py
+++ setup.py
@@ -13,7 +13,9 @@ ext_modules += cythonize(Extension(
     "mkp._algorithms_cy.mtm_cy", 
     sources=["cpp/mtm_c.cpp","python/mkp/_algorithms_cy/mtm_cy.pyx"], 
     include_dirs=['cpp/'],
-    language='c++'))
+    language='c++',
+    extra_compile_args=["-std=c++11"],
+    extra_link_args=["-std=c++11"]))
 
 
 setup(name='mkp',
