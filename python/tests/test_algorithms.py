4a4fb4aea169a9de9d5a78b8f79d4a465ee50816
Jesse Myrberg <jesse.myrberg@elisa.fi>
2020-06-13 16:50:50 +0300

    Add pip installation + travis

--- python/tests/test_algorithms.py
+++ python/tests/test_algorithms.py
@@ -1,153 +1,111 @@
 """Test cases for algorithms."""
 
 
-import time
+import pytest
 
-from mkp.algorithms import mtm
+from mkp.algorithms import mtm  # Compile the latest version inplace
 
 
-def test_mtm():
-    
-    start_tot = time.clock()
-    
-    print('-'*10 + ' TEST 01 ' + '-'*10)
-    start = time.clock()
-    p = [92, 57, 49, 68, 60, 43, 67, 84, 87, 72]
-    w = [23, 31, 29, 44, 53, 38, 63, 85, 89, 82]
-    c = [70, 127]
-    z,x,bt,_ = mtm(p, w, c)
-    print('Total profit: %d' % z)
-    print('Solution: %s' % x)
-    print('Number of backtracks performed: %d' % bt)
-    print('Time spent: %.5f' % round(time.clock()-start,5))
-    assert z == 333
-    assert x == [0,1,1,0,-1,-1,1,-1,-1,-1]
-    
-    print('\n' + '-'*10 + ' TEST 02 ' + '-'*10)
-    start = time.clock()
-    p = [110, 150, 70, 80, 30, 5]
-    w = [40, 60, 30, 40, 20, 5]
-    c = [65, 85]
-    z,x,bt,_ = mtm(p, w, c)
-    print('Total profit: %d' % z)
-    print('Solution: %s' % x)
-    print('Number of backtracks performed: %d' % bt)
-    print('Time spent: %.5f' % round(time.clock()-start,5))
-    assert z == 345
-    assert x == [1,0,-1,1,-1,0]
-                  
-    print('\n' + '-'*10 + ' TEST 03 ' + '-'*10)
-    start = time.clock()
-    p = [78, 35, 89, 36, 94, 75, 74, 79, 80, 16]
-    w = [18, 9, 23, 20, 59, 61, 70, 75, 76, 30]
-    c = [103, 156]
-    z,x,bt,_ = mtm(p, w, c)
-    print('Total profit: %d' % z)
-    print('Solution: %s' % x)
-    print('Number of backtracks performed: %d' % bt)
-    print('Time spent: %.5f' % round(time.clock()-start,5))
-    assert z == 452
-    assert x == [0,-1,0,1,1,0,-1,-1,1,-1]
-      
-    print('\n' + '-'*10 + ' TEST 04' + '-'*10)
-    start = time.clock()
-    p = [78, 35, 89, 36, 94, 75, 74, 79, 80, 16]
-    w = [18, 9, 23, 20, 59, 61, 70, 75, 76, 30]
-    c = [76, 110, 112]
-    z,x,bt,_ = mtm(p, w, c)
-    print('Total profit: %d' % z)
-    print('Solution: %s' % x)
-    print('Number of backtracks performed: %d' % bt)
-    print('Time spent: %.5f' % round(time.clock()-start,5))
-    assert z == 503
-    assert x == [1,1,1,2,1,2,-1,-1,0,2]
-      
-    print('\n' + '-'*10 + ' TEST 05' + '-'*10)
-    start = time.clock()
-    p = [78, 77, 35, 34, 33, 89, 88, 36, 35, 94, 93, 75, 74, 74, 73, 79, 78, 80, 79, 16, 15, 10]
-    w = [18, 18,  9,  9,  9, 23, 23, 20, 20, 59, 59, 61, 61, 70, 70, 75, 75, 76, 76, 30, 30, 40]
-    c = [80, 103, 109, 112]
-    z,x,bt,_ = mtm(p, w, c)
-    print('Total profit: %d' % z)
-    print('Solution: %s' % x)
-    print('Number of backtracks performed: %d' % bt)
-    print('Time spent: %.5f' % round(time.clock()-start,5))
-    assert z == 841
-    
-    print('\n' + '-'*10 + ' TEST 06' + '-'*10)
-    start = time.clock()
-    p = [78, 77, 35, 34, 89, 88, 36, 35, 94, 93, 75, 74, 74, 73, 79, 78, 80, 79, 16, 15, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-    w = [18, 18, 9, 9, 23, 23, 20, 20, 59, 59, 61, 61, 70, 70, 75, 75, 76, 76, 30, 30, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49]
-    c = [80, 90, 100, 110]
-    z,x,bt,_ = mtm(p, w, c)
-    print('Total profit: %d' % z)
-    print('Solution: %s' % x)
-    print('Number of backtracks performed: %d' % bt)
-    print('Time spent: %.5f' % round(time.clock()-start,5))
-    assert z == 779
-    
-    print('\n' + '-'*10 + ' TEST 07' + '-'*10)
-    start = time.clock()
-    p = [78, 77, 35, 34, 33, 89, 88, 36, 35, 94, 93, 75, 74, 74, 73, 79, 78, 80, 79, 16, 15, 10]
-    w = [18, 18,  9,  9,  9, 23, 23, 20, 20, 59, 59, 61, 61, 70, 70, 75, 75, 76, 76, 30, 30, 40]
-    c = [112, 109, 80, 103]
-    z,x,bt,_ = mtm(p[::-1], w[::-1], c)
-    print('Total profit: %d' % z)
-    print('Solution: %s' % x)
-    print('Number of backtracks performed: %d' % bt)
-    print('Time spent: %.5f' % round(time.clock()-start,5))
-    assert z == 841
-    
-    print('\n' + '-'*10 + ' TEST 08' + '-'*10)
-    start = time.clock()
-    p = [78, 77, 35, 34, 33, 89, 88, 36, 35, 94, 93, 75, 74, 74, 73, 79, 78, 80, 79, 16, 15, 10]
-    w = [18, 18,  9,  9,  9, 23, 23, 20, 20, 59, 59, 61, 61, 70, 70, 75, 75, 76, 76, 30, 30, 40]
-    c = [20, 40, 60, 80, 100]
-    z,x,bt,_ = mtm(p, w, c)
-    print('Total profit: %d' % z)
-    print('Solution: %s' % x)
-    print('Number of backtracks performed: %d' % bt)
-    print('Time spent: %.5f' % round(time.clock()-start,5))
-    assert z == 708
-    
-    print('\n' + '-'*10 + ' TEST 09' + '-'*10)
-    start = time.clock()
-    p = [78, 77, 35, 34, 33, 89, 88, 36, 35, 94, 93, 75, 74, 74, 73, 79, 78, 80, 79, 16, 15, 10]
-    w = [18, 18,  9,  9,  9, 23, 23, 20, 20, 59, 59, 61, 61, 70, 70, 75, 75, 76, 76, 30, 30, 40]
-    c = [20, 30, 35, 40, 45, 100]
-    z,x,bt,_ = mtm(p, w, c)
-    print('Total profit: %d' % z)
-    print('Solution: %s' % x)
-    print('Number of backtracks performed: %d' % bt)
-    print('Time spent: %.5f' % round(time.clock()-start,5))
-    assert z == 615
-    
-    print('\n' + '-'*10 + ' TEST 10' + '-'*10)
-    start = time.clock()
-    p = [63, 66, 65, 64, 64, 64, 64, 63, 64, 65, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6]
-    w = [420, 510, 510, 420, 420, 390, 420, 510, 420, 510, 270, 300, 330, 360, 390, 420, 450, 480, 510, 540, 570, 600]
-    c = [1320, 1320, 1320, 1320]
-    z,x,bt,_ = mtm(p, w, c)
-    print('Total profit: %d' % z)
-    print('Solution: %s' % x)
-    print('Number of backtracks performed: %d' % bt)
-    print('Time spent: %.5f' % round(time.clock()-start,5))
-    assert z == 649
-    
-    print('\n' + '-'*10 + ' TEST 11' + '-'*10)
-    start = time.clock()
-    p = [63, 66, 65, 64, 64, 64, 64, 63, 64, 65, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6]
-    w = [420, 510, 510, 420, 420, 390, 420, 510, 420, 510, 270, 300, 330, 360, 390, 420, 450, 480, 510, 540, 570, 600]
-    c = [1320, 1320, 1320, 1320]
-    z,x,bt,_ = mtm(p, w, c, 10000)
-    print('Total profit: %d' % z)
-    print('Solution: %s' % x)
-    print('Number of backtracks performed: %d' % bt)
-    print('Time spent: %.5f' % round(time.clock()-start,5))
-    assert z == 649
-    
-    print('\nTotal time: %.5f' % round(time.clock()-start_tot,5))
-    
-
-if __name__ == '__main__':
-    test_mtm()
+@pytest.mark.parametrize(
+    'params',
+    [
+        {
+            'p': [92, 57, 49, 68, 60, 43, 67, 84, 87, 72],
+            'w': [23, 31, 29, 44, 53, 38, 63, 85, 89, 82],
+            'c': [70, 127],
+            'z': 333,
+            'x': [0, 1, 1, 0, -1, -1, 1, -1, -1, -1]
+        },
+        {
+            'p': [110, 150, 70, 80, 30, 5],
+            'w': [40, 60, 30, 40, 20, 5],
+            'c': [65, 85],
+            'z': 345,
+            'x': [1, 0, -1, 1, -1, 0]
+        },
+        {
+            'p': [78, 35, 89, 36, 94, 75, 74, 79, 80, 16],
+            'w': [18, 9, 23, 20, 59, 61, 70, 75, 76, 30],
+            'c': [103, 156],
+            'z': 452,
+            'x': [0, -1, 0, 1, 1, 0, -1, -1, 1, -1]
+        },
+        {
+            'p': [78, 35, 89, 36, 94, 75, 74, 79, 80, 16],
+            'w': [18, 9, 23, 20, 59, 61, 70, 75, 76, 30],
+            'c': [76, 110, 112],
+            'z': 503,
+            'x': [1, 1, 1, 2, 1, 2, -1, -1, 0, 2]
+        },
+        {
+            'p': [78, 77, 35, 34, 33, 89, 88, 36, 35, 94, 93, 75, 74, 74, 73,
+                  79, 78, 80, 79, 16, 15, 10],
+            'w': [18, 18,  9,  9,  9, 23, 23, 20, 20, 59, 59, 61, 61, 70, 70,
+                  75, 75, 76, 76, 30, 30, 40],
+            'c': [80, 103, 109, 112],
+            'z': 841,
+            'x': None
+        },
+        {
+            'p': [78, 77, 35, 34, 89, 88, 36, 35, 94, 93, 75, 74, 74, 73, 79,
+                  78, 80, 79, 16, 15, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+            'w': [18, 18, 9, 9, 23, 23, 20, 20, 59, 59, 61, 61, 70, 70, 75, 75,
+                  76, 76, 30, 30, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49],
+            'c': [80, 90, 100, 110],
+            'z': 779,
+            'x': None
+        },
+        {
+            'p': [78, 77, 35, 34, 33, 89, 88, 36, 35, 94, 93, 75, 74, 74, 73,
+                  79, 78, 80, 79, 16, 15, 10],
+            'w': [18, 18,  9,  9,  9, 23, 23, 20, 20, 59, 59, 61, 61, 70, 70,
+                  75, 75, 76, 76, 30, 30, 40],
+            'c': [112, 109, 80, 103],
+            'z': 841,
+            'x': None
+        },
+        {
+            'p': [78, 77, 35, 34, 33, 89, 88, 36, 35, 94, 93, 75, 74, 74, 73,
+                  79, 78, 80, 79, 16, 15, 10],
+            'w': [18, 18,  9,  9,  9, 23, 23, 20, 20, 59, 59, 61, 61, 70, 70,
+                  75, 75, 76, 76, 30, 30, 40],
+            'c': [20, 40, 60, 80, 100],
+            'z': 708,
+            'x': None
+        },
+        {
+            'p': [78, 77, 35, 34, 33, 89, 88, 36, 35, 94, 93, 75, 74, 74, 73,
+                  79, 78, 80, 79, 16, 15, 10],
+            'w': [18, 18,  9,  9,  9, 23, 23, 20, 20, 59, 59, 61, 61, 70, 70,
+                  75, 75, 76, 76, 30, 30, 40],
+            'c': [20, 30, 35, 40, 45, 100],
+            'z': 615,
+            'x': None
+        },
+        {
+            'p': [63, 66, 65, 64, 64, 64, 64, 63, 64, 65, 3, 3, 3, 4, 4, 4, 5,
+                  5, 5, 6, 6, 6],
+            'w': [420, 510, 510, 420, 420, 390, 420, 510, 420, 510, 270, 300,
+                  330, 360, 390, 420, 450, 480, 510, 540, 570, 600],
+            'c': [1320, 1320, 1320, 1320],
+            'z': 649,
+            'x': None
+        },
+        {
+            'p': [63, 66, 65, 64, 64, 64, 64, 63, 64, 65, 3, 3, 3, 4, 4, 4, 5,
+                  5, 5, 6, 6, 6],
+            'w': [420, 510, 510, 420, 420, 390, 420, 510, 420, 510, 270, 300,
+                  330, 360, 390, 420, 450, 480, 510, 540, 570, 600],
+            'c': [1320, 1320, 1320, 1320],
+            'z': 649,
+            'x': None
+        }
+    ]
+)
+def test_mtm(params):
+    expected_z = params.pop('z')
+    expected_x = params.pop('x')
+    z, x, bt, _ = mtm(**params)
+    assert z == expected_z
+    if expected_x:
+        assert x == expected_x
--- setup.py
+++ setup.py
@@ -1,32 +1,48 @@
-try:
-    from Cython.Build import cythonize,build_ext
-except:
-    raise ImportError("You must have cython installed in order to use mkp!") 
+from setuptools import Extension, setup
 
-import os
 
-from distutils.core import setup, Extension
+with open('README.md', 'r') as f:
+    long_description = f.read()
 
+with open('VERSION', 'r') as f:
+    version = f.read().strip()
 
 ext_modules = []
-ext_modules += cythonize(Extension(
-    "mkp._algorithms_cy.mtm_cy", 
-    sources=["cpp/mtm_c.cpp","python/mkp/_algorithms_cy/mtm_cy.pyx"], 
+ext_modules += [Extension(
+    "mkp._algorithms_cy.mtm_cy",
+    sources=["cpp/mtm_c.cpp", "python/mkp/_algorithms_cy/mtm_cy.pyx"],
     include_dirs=['cpp/'],
     language='c++',
-    extra_compile_args=["-std=c++1y"]))
+    extra_compile_args=["-std=c++1y"]
+)]
 
-
-setup(name='mkp',
-      version='1.0',
-      description='Algorithms for Multiple Knapsack Problem (MKP)',
-      author='Jesse Myrberg',
-      author_email='jesse.myrberg@gmail.com',
-      url='https://github.com/jmyrberg/mkp',
-      license='MIT',
-      packages=['mkp','mkp._algorithms_cy'],
-      package_dir={
-          'mkp': 'python/mkp',
-      },
-      ext_modules=ext_modules)
-
+setup(
+    name='mkp',
+    version=version,
+    license='MIT',
+    description='Algorithms for Multiple Knapsack Problem (MKP)',
+    long_description=long_description,
+    author='Jesse Myrberg',
+    author_email='jesse.myrberg@gmail.com',
+    url='https://github.com/jmyrberg/mkp',
+    keywords=['algorithm', 'multiple', 'knapsack', 'optimization'],
+    setup_requires=[
+        'cython>=0.26.1'
+    ],
+    install_requires=[
+        'cython>=0.26.1',
+        'numpy>=1.13.3',
+        'pandas>=0.20.3'
+    ],
+    packages=['mkp', 'mkp._algorithms_cy'],
+    package_dir={
+        'mkp': 'python/mkp',
+    },
+    ext_modules=ext_modules,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License'
+    ]
+)
