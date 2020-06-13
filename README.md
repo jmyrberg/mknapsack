8c7f2a851c62bc759ddb1ec3464e33e0d006763b
Jesse Myrberg <jesse.myrberg@gmail.com>
2020-06-13 17:14:12 +0300

    Add pip installation (#3)
    * Add pip installation + travis
    
    * mkp --> mknapsack
    
    * Add before_script

--- README.md
+++ README.md
@@ -1,32 +1,35 @@
-# mkp
+# mknapsack
 
 Algorithms for solving the [Multiple 0-1 Knapsack Problem](http://www.or.deis.unibo.it/kp/Chapter6.pdf) (MKP).
 Currently, only the [MTM algorithm by S. Martello and P. Toth (1981)](https://www.sciencedirect.com/science/article/pii/0166218X81900056) is implemented, 
 which guarantees an exact solution. This repository contains a Python interface to C++ implementation of the algorithm.
 
+
 ## Installation
 
-1. Clone the repository ```git clone https://github.com/jmyrberg/mkp```
-2. Run ```pip install -r requirements.txt``` at the root of repository
-3. Run ```python setup.py install``` for installation
+`pip install mknapsack`
+
 
 ## Example usage
 Given ten items with the following profits and weights:
+
 ```python
 profits = [78, 35, 89, 36, 94, 75, 74, 79, 80, 16]
 weights = [18, 9, 23, 20, 59, 61, 70, 75, 76, 30]
 ```
 
 and two knapsacks with the following capacities:
+
 ```python
 capacities = [90, 100]
 ```
 
 How should we assign these items to knapsacks in order to maximize the profit?
-```python
-from mkp.algorithms import mtm
 
-z,x,bt,glopt = mtm(profits, weights, capacities)
+```python
+from mknapsack.algorithms import mtm
+
+z, x, bt, glopt = mtm(profits, weights, capacities)
 print('Total profit: %d' % z)
 print('Solution: %s' % x)
 print('Number of backtracks performed: %d' % bt)
@@ -34,6 +37,7 @@ print('Global optimum: %s' % glopt)
 ```
 
 ## References
+
 * [MTM algorithm by Martello and Toth](http://people.sc.fsu.edu/~jburkardt/f77_src/knapsack/knapsack.f) (Fortran)
 * [MTHM and MTHG algorithms by Jeff Hetherly](https://github.com/jhetherly/python_knapsack) (Python)
 
--- python/mknapsack/algorithms.py
+++ python/mknapsack/algorithms.py
@@ -6,21 +6,21 @@ from functools import reduce
 
 import numpy as np
 import pandas as pd
- 
+
 try:
-    from mkp._algorithms_cy.mtm_cy import cyMTMSolver
-except:
-    raise ImportError("Could not import module - most likely Cython / C++ "
-                      "was not compiled properly")
-  
-  
-def greatest_common_divisor(l):
-    return reduce(gcd, l)
-  
-  
+    from mknapsack._algorithms_cy.mtm_cy import cyMTMSolver
+except ModuleNotFoundError:
+    raise ImportError('Could not import module - most likely Cython / C++ '
+                      'was not compiled properly')
+
+
+def greatest_common_divisor(number):
+    return reduce(gcd, number)
+
+
 def mtm(p, w, c, max_bt=-1, max_time=3600):
     """Solves Multiple 0-1 Knapsack Problem with MTM algorithm.
-      
+
     Args:
         p (list): Item profits.
         w (list): Item weights.
@@ -29,81 +29,83 @@ def mtm(p, w, c, max_bt=-1, max_time=3600):
             which is unlimited.
         max_time (int): Number of seconds after which the best solution so far
             is returned. Defaults to 3600.
-          
+
     Returns:
+        Tuple of the following:
         x (list): Assigned knapsacks for each item. Knapsack '-1' indicates
             that the item is not assigned to any knapsack.
-        glopt (bool): Whether the given solution is guaranteed to be global optimum or not.
+        glopt (bool): Whether the given solution is guaranteed to be global
+            optimum or not.
         z (int): Total profit.
         bt (int): Number of backtracks performed by the algorithm.
-      
+
     Raises:
         ValueError, if inputs are of incorrect type or size.
-          
+
     References:
         S. Martello, P. Toth
         A Bound and Bound algorithm for the zero-one multiple knapsack problem
         Discrete Applied Mathematics, 3 (1981), pp. 257-288
     """
     # Validate inputs
-    if not all(isinstance(vec,list) for vec in [p,w,c]):
-        raise ValueError("All inputs are not of type 'list'")
+    if not all(isinstance(vec, list) for vec in [p, w, c]):
+        raise ValueError('All inputs are not of type "list"')
     if len(p) != len(w):
-        raise ValueError("Profit and weight lengths are not equal (%d != %d)" %
-                         len(p),len(w))
-    if not isinstance(max_bt,int):
-        raise ValueError("Parameter 'max_bt' must of type 'int'")
-    if not isinstance(max_time,int):
-        raise ValueError("Parameter 'max_time' must of type 'int'")
+        raise ValueError('Profit and weight lengths are not equal (%d != %d)'
+                         % len(p), len(w))
+    if not isinstance(max_bt, int):
+        raise ValueError('Parameter ``max_bt`` must of type "int"')
+    if not isinstance(max_time, int):
+        raise ValueError('Parameter ``max_time`` must of type "int"')
     if max_time < 1:
-        raise ValueError("Parameter 'max_time' must be positive")
-     
+        raise ValueError('Parameter ``max_time`` must be positive')
+
     items = np.arange(len(p), dtype=int)
     ksacks = np.arange(len(c), dtype=int)
-    
+
     # Scale the problem
     scale = greatest_common_divisor(w + c)
-    
+
     # Decreasing profit/weight ratio for items
     p_ar = np.array(p, dtype=int)
     w_ar = np.array(w, dtype=int) / scale
     pw_ord = np.argsort(p_ar / w_ar)[::-1]
     pw_map = np.zeros(len(p), dtype=int)
     pw_map[pw_ord] = items
-    
+
     # Increasing capacity for knapsacks
     c_ar = np.array(c, dtype=int) / scale
     c_ord = np.argsort(c_ar)
-    c_map = dict((i,v) for i,v in enumerate(c_ord))
+    c_map = dict((i, v) for i, v in enumerate(c_ord))
     c_map[-1] = -1
-    
+
     # Sort arrays
     p_srt = p_ar[pw_ord].tolist()
     w_srt = w_ar[pw_ord].tolist()
     c_srt = c_ar[c_ord].tolist()
-    
+
     # Solve
     res = cyMTMSolver(p_srt, w_srt, c_srt, max_bt, max_time).solve()
     x = np.array(res[:-3])
     glopt = res[-3]
     z = res[-2]
     bt = res[-1]
-    
+
     # Sort items and knapsacks to their original order
     x = [c_map[i] for i in x[pw_map]]
-    
+
     # Ensure solution validity
     x_ar = np.array(x)
     df = pd.DataFrame({'i': x_ar, 'j': items, 'p': p_ar, 'w': w_ar * scale})
-    df = df.merge(pd.DataFrame({'i': ksacks, 'c': c_ar * scale}), 
+    df = df.merge(pd.DataFrame({'i': ksacks, 'c': c_ar * scale}),
                   on='i', how='left')
     df['c'] = df['c'].fillna(-1).astype(int)
     df = df.groupby('i').agg({'p': np.sum, 'w': np.sum, 'c': np.max})
     df['valid'] = (df['c'] >= df['w']).astype(int)
-    if z != df.loc[df.index>=0,'p'].sum():
-        raise ValueError("Solution value not matching " 
-                         "the profits of chosen items")
+    if z != df.loc[df.index >= 0, 'p'].sum():
+        raise ValueError('Solution value not matching the profits of '
+                         'chosen items')
     if df['valid'].sum() != ksacks.shape[0]:
-        raise ValueError("Solution not valid:\n%s" % df)
-    
-    return z,x,bt,glopt
+        raise ValueError('Solution not valid:\n%s' % df)
+
+    return z, x, bt, glopt
